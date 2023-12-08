7c896afc26c22bb49e0c5996466eaa14ca0c5b4d
Aung-xiao <2557160927@qq.com>
2023-12-08 20:19:12 +0800

    v1.0.0

--- src/asr5_control.cpp
+++ src/asr5_control.cpp
@@ -1,37 +1,111 @@
 //
 // Created by aung on 23-12-8.
 //
+//ROS
 #include "ros/ros.h"
-#include "std_msgs/String.h"
 #include <sstream>
+#include<thread>
 
+//Other
+#include <cstdio>
 #include"ar5.h"
-#include"json.hpp"
-//#include "websocket_endpoint.h"
+#include "websocket_endpoint.h"
+
+using namespace std;
+unique_ptr<ar5> Arm = make_unique<ar5>();
+void sokect_thread()
+{
+    std::string input;
+    kagula::websocket_endpoint endpoint;
+    endpoint.connect("ws://127.0.0.1:1880");
+
+    while (1)
+    {
+        string s;
+        int a, b = 0;
+        cout << "1 上电、2 下电、3 获取状态信息、 4 获取位置点信息、 5 机械笔启动、6 机械笔停止 7 机械笔自由拖动 8 停止机械笔自由拖动 9 机械臂xyz给进" << endl;
+        cout << "10机械臂关节给进  11 机械臂旋转给进 12暂停运动 13恢复运动  14停止运动" << endl;
+        printf("Input integer number:");
+        scanf("%d", &a);
+        switch (a) {
+            case 1: printf("上电\n");
+                s = Arm->To_CBOR(Arm->Power(true));
+                endpoint.send(s);
+                break;
+            case 2: printf("下电\n");
+                s = Arm->To_CBOR(Arm->Power(false));
+                endpoint.send(s);
+                break;
+            case 3: printf("获取状态信息\n");
+                s = Arm->To_CBOR(Arm->GetInfo());
+                endpoint.send(s);
+                break;
+            case 4: printf("获取位置点信息\n");
+                s = Arm->To_CBOR(Arm->GetGeom());
+                endpoint.send(s);
+                break;
+            case 5: printf("机械臂启动\n");
+                s = Arm->To_CBOR(Arm->Start(true));
+                endpoint.send(s);
+                break;
+            case 6: printf("机械臂停止\n");
+                s = Arm->To_CBOR(Arm->Start(false));
+                endpoint.send(s);
+                break;
+            case 7: printf("机械臂自由托动\n");
+                s = Arm->To_CBOR(Arm->SwitchToFree(true));
+                endpoint.send(s);
+                break;
+            case 8:printf("停止机械臂自由托动\n");
+                s = Arm->To_CBOR(Arm->SwitchToFree(false));
+                endpoint.send(s);
+                break;
+            case 9:printf("机械臂给进\n");
+                s=Arm->To_CBOR(Arm->startCartFeedTrans(1, 0.5));
+                endpoint.send(s);
+                break;
+            case 10:printf("机械臂关节给进\n");
+                s=Arm->To_CBOR(Arm->startJointFeedRot(0, 0.5));
+                endpoint.send(s);
+                break;
+            case 11:printf("机械臂旋转给进\n");
+                s = Arm->To_CBOR(Arm->startCartFeedRot(1, 0.5));
+                endpoint.send(s);
+                break;
+            case 12:printf("暂停运动\n");
+                s = Arm->To_CBOR(Arm->pauseMove());
+                endpoint.send(s);
+                break;
+            case 13:printf("恢复运动\n");
+                s = Arm->To_CBOR(Arm->resumeMove());
+                endpoint.send(s);
+                break;
+            case 14:printf("停止运动\n");
+                s = Arm->To_CBOR(Arm->abortMove());
+                endpoint.send(s);
+                break;
+            default:printf("error\n");
+                b = 1;
+                break;
+        }
+        if (b == 1)
+        {
+            break;
+        }
+
+    }
+    endpoint.close();
+}
+
 
 int main(int argc, char  *argv[])
 {
     setlocale(LC_ALL,"");
     ros::init(argc,argv,"talker");
     ros::NodeHandle nh;
-    ros::Publisher pub = nh.advertise<std_msgs::String>("chatter",10);
-    std_msgs::String msg;
-    std::string msg_front = "Hello 你好！";
-    int count = 0;
-    ros::Rate r(1);
 
-    while (ros::ok())
-    {
-
-        std::stringstream ss;
-        ss << msg_front << count;
-        msg.data = ss.str();
-        pub.publish(msg);
-        ROS_INFO("发送的消息:%s",msg.data.c_str());
-
-        r.sleep();
-        ros::spinOnce();
-    }
+    thread web_socket_task(sokect_thread);
+    web_socket_task.join();
 
     return 0;
 }
