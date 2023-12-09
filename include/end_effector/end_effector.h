09d1e58d0b15db82ac9975f85cb0b4dec601da93
Aung-xiao <2557160927@qq.com>
2023-12-09 17:27:24 +0800

    v2.0.0:add can and serial demos

---
+++ include/end_effector/end_effector.h
@@ -0,0 +1,9 @@
+//
+// Created by aung on 2023/12/8.
+//
+
+#ifndef SRC_END_EFFECTOR_H
+#define SRC_END_EFFECTOR_H
+
+
+#endif //SRC_END_EFFECTOR_H
---
+++ include/end_sensor/end_sensor.h
@@ -0,0 +1,8 @@
+//
+// Created by aung on 2023/12/9.
+//
+
+#ifndef SRC_END_SENSOR_H
+#define SRC_END_SENSOR_H
+
+#endif //SRC_END_SENSOR_H
