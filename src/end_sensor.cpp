144e081e078713206302b9ba5f0c7f55f16d64f5
Aung-xiao <2557160927@qq.com>
2023-12-31 10:29:38 +0800

    read end_sensor value (not test)

--- src/end_sensor.cpp
+++ src/end_sensor.cpp
@@ -5,8 +5,6 @@
 #include <serial/serial.h>
 #include <iostream>
 #include "end_sensor.h"
-#include <QByteArray>
-#include <QString>
 
 int main(int argc, char** argv)
 {
@@ -18,6 +16,7 @@ int main(int argc, char** argv)
     sp.setPort("/dev/ttyUSB0");
     sp.setBaudrate(115200);
     sp.setTimeout(to);
+    int value{};
 
     try
     {
@@ -44,21 +43,26 @@ int main(int argc, char** argv)
         if (sp.available())
         {
             std::string dataStr = sp.read(sp.available());
-            QByteArray data = QByteArray::fromStdString(dataStr);
-            QString hexStr=data.mid(3, 4).toHex();
-            bool conversionOK;
-            auto intForceValue = static_cast<qint16>(hexStr.toUInt(&conversionOK, 16));
-            if (!conversionOK) {
-                ROS_ERROR_STREAM("Conversion fail .");
-                return -1;
-            }
-            if (intForceValue > 0x7FFF) {
-                intForceValue -= 0x10000;
-            }
-            double doubleForceValue = static_cast<double>(intForceValue)*0.01;
-            ROS_INFO("end_sensor value: %f \r\n",doubleForceValue);
+            ROS_INFO("end_sensor str: %s \r\n",dataStr.c_str ()); //01 03 02 19 98 B2 7E （这里的19 98 是我们要的）
+            value=*(uint16_t*)(&dataStr+1);
+            ROS_INFO("end_sensor value: %d \r\n",value);
+
+//            QByteArray data = QByteArray::fromStdString(dataStr);
+//            QString hexStr=data.mid(3, 4).toHex();
+//            bool conversionOK;
+//            auto intForceValue = static_cast<qint16>(hexStr.toUInt(&conversionOK, 16));
+//            if (!conversionOK) {
+//                ROS_ERROR_STREAM("Conversion fail .");
+//                return -1;
+//            }
+//            if (intForceValue > 0x7FFF) {
+//                intForceValue -= 0x10000;
+//            }
+//            double doubleForceValue = static_cast<double>(intForceValue)*0.01;
+//            ROS_INFO("end_sensor value: %f \r\n",doubleForceValue);
         }
-        sp.write("010300500002C41A");
+//        sp.write("010300500002C41A");
+        sp.write("01039C400002EB8F");
         loop_rate.sleep();
     }
 
