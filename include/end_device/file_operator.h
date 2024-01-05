48af2377fe514d0d47faceb1cb199a66c54863c0
Aung-xiao <2557160927@qq.com>
2024-01-05 19:26:25 +0800

    try to add file operator (not finish)

---
+++ include/end_device/file_operator.h
@@ -0,0 +1,22 @@
+//
+// Created by aung on 2024/1/5.
+//
+
+#ifndef SRC_FILE_OPERATOR_H
+#define SRC_FILE_OPERATOR_H
+
+
+#include <string>
+
+namespace fille_operator
+{
+class fileOperator
+{
+public:
+    void writeFile(double max_force,double min_force,double interval_time);
+
+};
+
+} // fille_operator
+
+#endif //SRC_FILE_OPERATOR_H
---
+++ src/end_device/file_operator.cpp
@@ -0,0 +1,8 @@
+//
+// Created by aung on 2024/1/5.
+//
+
+#include "file_operator.h"
+
+namespace fille_operator {
+} // filleOperator
