5210c44cdf2499284bc911a6476fc2d62d44013c
Aung-xiao <2557160927@qq.com>
2024-01-08 19:20:15 +0800

    add putter node (not test)

---
+++ src/putter_node.cpp
@@ -0,0 +1,26 @@
+//
+// Created by aung on 2024/1/8.
+//
+#include "fsm.h"
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv,"end_device");
+    ros::NodeHandle nh;
+    ros::Rate r(20);
+    setlocale(LC_ALL, "");
+    setlocale(LC_CTYPE, "zh_CN.utf8");
+
+    end_putter::endPutter end_putter;
+    file_operator::fileOperator file_operator;
+
+    end_putter.initPutter();
+
+    while(ros::ok()&&end_putter.putterisOpen())
+    {
+        if(end_putter.send_string("1 1 45"))
+            break;
+        r.sleep();
+    }
+    return 0;
+}
