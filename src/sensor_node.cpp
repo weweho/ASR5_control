d672f1020955ae0fb6981f309398e08e6d198cac
Aung-xiao <2557160927@qq.com>
2024-01-10 17:24:49 +0800

    bigger freq for sensor_get

--- src/sensor_node.cpp
+++ src/sensor_node.cpp
@@ -10,7 +10,7 @@ int main(int argc, char** argv)
     ros::NodeHandle nh;
     setlocale(LC_ALL, "");
     setlocale(LC_CTYPE, "zh_CN.utf8");
-    int freq=20;
+    int freq=50;
     ros::Rate r(freq);
 
     end_sensor::endSensor end_sensor;
@@ -22,9 +22,9 @@ int main(int argc, char** argv)
     ros::Publisher value_pub = nh.advertise<std_msgs::Float64>("/sensor_value", 1000);
     while(ros::ok())
     {
-        end_sensor.getSensorData(&sensor_value_.data);
-        value_pub.publish(sensor_value_);
-//        fsm.testSensorData(&end_sensor, &file_operator , freq);
+//        end_sensor.getSensorData(&sensor_value_.data);
+//        value_pub.publish(sensor_value_);
+        fsm.testSensorData(&end_sensor, &file_operator , freq);
         r.sleep();
     }
     return 0;
